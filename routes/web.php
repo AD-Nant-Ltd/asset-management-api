dfdb7bb80ffd41cea0ecbb4ce89150b92f2c5ce4
Adam Dolphin <adam.dolphin@nantltd.co.uk>
2026-09-26 17:13:42 +0100

    feat: add route redirect

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return Auth::check()
+        ? redirect()->route('dashboard')
+        : redirect()->route('login');
+});
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'create'])
